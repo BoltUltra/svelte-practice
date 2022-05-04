0fd48cb49b923a3a65f17019f863c88909408731
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-05-04 13:11:38 +0100

    '...'

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,6 +6,7 @@
 	<h2>About us</h2>
 	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, quasi asperiores! Necessitatibus quasi fuga laudantium, rem, reiciendis nesciunt quibusdam animi pariatur suscipit, est porro at. Quibusdam blanditiis deserunt sunt necessitatibus praesentium consectetur soluta recusandae ad libero facere, expedita ducimus quam tempore dolorem, impedit perspiciatis aspernatur neque, doloribus vel consequatur eum.</p>
 	<a href="/">Home</a>
+	<a href="/guides">Guides</a>
 </div>
 
 <style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,7 @@
 	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero earum doloremque provident repellat eius maxime in, optio ab! Temporibus consequuntur quae facilis fuga nulla beatae atque earum amet reprehenderit recusandae repudiandae nisi facere repellat obcaecati aut quam repellendus et velit ad, necessitatibus, eos molestias dolor. Eos aspernatur totam at sit.</p>
 
 	<a href="/about">About</a>
+	<a href="/guides">Guides</a>
 </div>
 
 <style>
